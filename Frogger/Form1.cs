9f9773221a5725b78d48d575be0f97bdd64788b8
Plankp T. <plankp@outlook.com>
2016-06-01 05:52:15 -0400

    Improved tile generation

--- Frogger/Form1.cs
+++ Frogger/Form1.cs
@@ -100,7 +100,7 @@ namespace Frogger
                 PictureBox pb = new PictureBox();
                 pb.Image = imgs.Images[0];
                 pb.Size = new Size(32, 32);
-                pb.Location = new Point(0, (int)Math.Floor(random.NextDouble() * 4 * QUARTER_Y));
+                pb.Location = new Point(0, (int)Math.Floor(random.NextDouble() * 4 * (QUARTER_Y - 16)) + 16);
                 this.Controls.Add(pb);
                 cars.Add(pb);
             }
